c871d97b37000e528ab2eb31ad74375f87740d06
Paul Treanor <paul_treanor@rapid7.com>
2025-08-05 10:41:48 +0100

    Override jest axios module override to use commonjs version

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
     transform: {'^.+\\.ts?$': 'ts-jest'},
     testEnvironment: 'node',
     testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    moduleNameMapper: {
+        '^axios$': 'axios/dist/node/axios.cjs'
+    }
   };
